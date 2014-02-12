073ad54da52ca553167ee217a0c988390a7cd6c5
crossgate9 <crossgate9@gmail.com>
2014-02-12 15:38:40 +0800

    [add] check status shell script

---
+++ tools/server/status.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#USAGE: ./status.sh www.example.com status@example.com
+
+url=$1
+receipt=$2
+echo $url
+status=`curl -sL -w "%{http_code} %{url_effective}\\n" $url -o /dev/null | awk '{print $1}'`
+echo $status
+if [ "$status" = "200" ]; then
+  echo 'OK'
+else
+  echo `date`": Server Down" | mail -s "$url-Status" $receipt
+fi
